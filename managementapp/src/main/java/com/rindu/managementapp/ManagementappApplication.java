097b2e12b25e366c5f7f0f81c2825f3640d63dfe
Rindu Tegar Senjawati <rindutegar21@gmail.com>
2024-10-01 00:01:28 +0700

    setup skeleton project b

---
+++ managementapp/src/main/java/com/rindu/managementapp/ManagementappApplication.java
@@ -0,0 +1,13 @@
+package com.rindu.managementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementappApplication.class, args);
+	}
+
+}
---
+++ managementapp/src/test/java/com/rindu/managementapp/ManagementappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rindu.managementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
